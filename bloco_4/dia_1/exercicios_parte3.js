3efd5a6dd90c8ec485298649f4aa63b1afa2a909
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-02-25 10:19:06 -0500

    exercicio parte3: comparando 3 numeros

---
+++ bloco_4/dia_1/exercicios_parte3.js
@@ -0,0 +1,13 @@
+let a = 300;
+let b = 70;
+let c = 10;
+
+if(a > b && a > c){
+    console.log('A maior')
+}
+else if(b > a && b > c){
+    console.log('B maior')
+}
+else{
+    console.log('C maior')
+}

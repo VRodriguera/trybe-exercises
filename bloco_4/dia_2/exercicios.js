c302637be1042ef64a63c2a0cf88825e80c40f10
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-02-26 09:09:52 -0500

    exercicio 2:imprime a soma de todos os valores contidos no array

--- bloco_4/dia_2/exercicios.js
+++ bloco_4/dia_2/exercicios.js
@@ -1,3 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
 
-console.log(numbers);
+for(let i = 0; i < numbers.length; i++){
+   total += numbers[i]
+}
+
+console.log(numbers); 
+console.log(total);

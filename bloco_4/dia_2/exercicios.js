e7dab1704183884100c4f5f4cc99b8b933df0c43
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-02-26 09:45:49 -0500

    exercicio 5:imprime o maior valor dentro do array

--- bloco_4/dia_2/exercicios.js
+++ bloco_4/dia_2/exercicios.js
@@ -1,21 +1,25 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
+let maior = 0;
 
+//for(let i = 0; i < numbers.length; i++){
+   //total += numbers[i]
+//}
 for(let i = 0; i < numbers.length; i++){
-   total += numbers[i]
-}
+    if(numbers[i] > maior){
+        maior = numbers[i]
+    }
+ }
+ console.log(maior)
 
-let media = total / numbers.length
+//let media = total / numbers.length
 
-if(media > 20){
-    console.log('valor maior que 20')
-}
-else{
-    console.log('valor menor que 20')
-}
-
-
-
+//if(media > 20){
+  //  console.log('valor maior que 20')
+//}
+//else{
+  //  console.log('valor menor que 20')
+//}
 
 //console.log(numbers.length); 
 //console.log(total);

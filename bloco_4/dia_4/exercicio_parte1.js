ea92555c305ae6b4f482363f7b0705068b90441d
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-02 09:57:33 -0300

    exercicio 1 oa 5 completos

--- bloco_4/dia_4/exercicio_parte1.js
+++ bloco_4/dia_4/exercicio_parte1.js
@@ -2,5 +2,39 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  };
-  console.log('Bem vinda, ' + info.personagem);
+    recorrente: "sim",
+};
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "sim",
+};
+
+//console.log('Bem vinda, ' + info.personagem);
+
+for (let i in info) {
+    //console.log(info)
+}
+
+for (let key in info) {
+    //console.log(key)
+}
+
+for (let i in info) {
+    
+    if (i == "recorrente" && info2.recorrente == 'sim' && info.recorrente == 'sim' ){
+        console.log('Ambos recorrentes')
+    }
+    else{
+        console.log(info[i] + ' e ' + info2[i])
+    }   
+}
+
+/*ambosRecorentes (info.recorrente, info2.recorrente)
+
+function ambosRecorentes (a , b) {
+    if (a == "sim" && b == "sim"){
+     console.log('Ambos recorrentes')
+    }
+}*/

513943a4b9fb0aa07d659297317a7ddd33873420
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-17 20:27:01 -0300

    exercicio dia 6.1 completo

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -27,19 +27,91 @@ let recebeEstado = [
     { 'SP': 'São Paulo' },
     { 'SE': 'Sergipe' },
     { 'TO': 'Tocantins' }
-  ];
+];
 
 function countries(recebeEstado) {
 
     for (let index = 0; index < recebeEstado.length; index += 1) {
 
-//console.log(listEstados);
         let varParent = document.querySelector('#estados')
         let varSon = document.createElement('option');
         varSon.value = Object.keys(recebeEstado[index]);
         varSon.innerText = Object.values(recebeEstado[index]);
         varParent.appendChild(varSon);
+
     }
-}botaoEstados.addEventListener('click', () => {
-    countries(recebeEstado);
+}
+window.addEventListener('load', countries(recebeEstado));
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+function checkData() {
+
+    let dataTest = '13/03/2021';
+    let dia, mes, ano = '';
+
+    dia = dataTest.split('/')[0];
+    mes = dataTest.split('/')[1];
+    ano = dataTest.split('/')[2];
+
+    
+    if (dia.length != 2 || mes.length != 2 || ano.length != 4) {
+        alert('Formato invalido!')
+        return false;
+    }  
+    if (dia < 0 || dia > 31) {
+        alert('Dia informado invalido!')
+        return false;
+    }
+    if (mes < 0 || mes > 12) {
+        alert('Mês informado invalido!')
+        return false;
+    }
+    if (ano < 0) {
+        alert('Ano informado invalido!')
+        return false;
+    }
+    return true;
+}
+checkData()
+
+function submitForm () {
+
+    let inputValue = document.getElementsByTagName('input');
+    let varParent = document.querySelector('#paragrafoItem')
+    let varSon = document.createElement('div');
+    varSon.setAttribute('id', 'minhaDiv')
+    varParent.appendChild(varSon);
+    let arrTest = [];
+    
+    for (let index in inputValue) {
+
+       if (inputValue[index] != undefined) {
+        arrTest.push(inputValue[index].value);
+       }
+    }
+
+    varSon.innerText = arrTest.join(' ')
+    console.log(varSon);
+}
+const buttonSubmit = document.getElementById('submit');
+buttonSubmit.addEventListener('click', () =>{
+      submitForm ();
 });
+
+function clear () {
+
+
+    document.getElementById('minhaDiv').innerText = '';
+
+     
+     document.getElementById("form").reset();
+     console.log('Olha eu aqui'); 
+
+}
+const buttonClear = document.getElementById('clearForm');
+buttonClear.addEventListener('click', () =>{
+    clear ();
+});   
